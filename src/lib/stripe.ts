f5848f9b86d5995a424e926f942e009d496f7d46
GORDO199 <marcinyaarea1@gmail.com>
2026-04-02 21:27:43 -0400

    Fix typescript error with Stripe API Version

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from "stripe";
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY || "sk_test_mock_123456789";
 
 export const stripe = new Stripe(stripeSecretKey, {
-  apiVersion: "2024-12-18.acacia", // Adjust depending on your exact stripe version needs
+  apiVersion: "2026-03-25.dahlia", // Adjust depending on your exact stripe version needs
   appInfo: {
     name: "Squito AI Dashboard",
     version: "1.0.0",
