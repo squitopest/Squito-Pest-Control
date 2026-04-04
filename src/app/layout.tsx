713ed22295a9aa809d5ff0c976cb2bb9647956cb
GORDO199 <marcinyaarea1@gmail.com>
2026-04-04 18:00:28 -0400

    Minor layout whitespace cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  
+
   const jsonLd = {
     "@context": "https://schema.org",
     "@type": "PestControl",
